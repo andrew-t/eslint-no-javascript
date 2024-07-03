fc7c17f38d304f336d642550896996a84683a995
Andrew Taylor <andrew@andrewt.net>
2024-07-03 20:16:23 +0100

    add test repo

---
+++ test/eslint.config.js
@@ -0,0 +1,20 @@
+import noJs from "eslint-no-javascript";
+
+export default [
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            sourceType: "commonjs",
+            ecmaVersion: "latest",
+            parserOptions: {
+                ecmaVersion: 10,
+                sourceType: "module"
+            },
+            sourceType: "module"
+        },
+        plugins: {"eslint-no-javascript": noJs},
+        rules: {
+            "eslint-no-javascript/no-javascript": "error"
+        }
+    }
+];
